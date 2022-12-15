e78ccfd58906d70c8768aba8001e0b04c228a40e
Linus <56478468+Lin-creator@users.noreply.github.com>
2022-12-15 19:30:53 +0100

    linus frontend with integration (#36)

--- Chatbot_Program/actions/actions.py
+++ Chatbot_Program/actions/actions.py
@@ -25,3 +25,38 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# To run install Flask & flask-cors with:
+# "pip install Flask, flask-cors"
+
+from sys import displayhook
+from typing import Any, Text, Dict, List
+from pyparsing import nestedExpr
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import json
+
+class ActionGetRecipy(Action):
+    
+    def name(self):
+        return "action_get_recipe"
+
+    def run(self, dispatcher, tracker, domain):
+        name = "Alabama Slammer"        
+        url = "https:\/\/www.thecocktaildb.com\/images\/media\/drink\/jntghf1606771886.jpg"
+        ingredient1 = "Southern Comfort"
+        ingredient2 = "Amaretto"
+        ingredient3 = "Sloe gin"
+        ingredient4 = "Lemon juice"
+        inst = "Alle Zutaten (au\u00dfer Zitronensaft) in einem Highball-Glas \u00fcber Eis gie\u00dfen. Umr\u00fchren, einen Schuss Zitronensaft hinzuf\u00fcgen und servieren."
+        sepIngredients = ","
+        sepInfo=";"
+        ingredients = [ingredient1, ingredient2, ingredient3, ingredient4]
+        infoArray = [name, url, sepIngredients.join(ingredients), inst]
+        message = sepInfo.join(infoArray) + " | "
+        print(message)
+        dispatcher.utter_message("-rec: " + message + message + message)
+
+        return[]
