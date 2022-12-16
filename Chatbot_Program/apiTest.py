fc82c328a89df2f4de9afe1de90fc0a974dc3396
Linus <56478468+Lin-creator@users.noreply.github.com>
2022-12-16 07:57:40 +0100

    Test (#41)
    * linus frontend with integration (#36)
    
    * implement actions, refactor code (#37)
    
    * Una1 (#38)
    
    * implement actions, refactor code
    
    * implement stories
    
    * 1. Deployment
    
    Co-authored-by: linus <l.pihan@conext-eos.com>

--- Chatbot_Program/apiTest.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-base_api_url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i="
-cocktailID = "11000"
-api_url = base_api_url + cocktailID
-print(api_url)
-response = requests.post(api_url)
-print(response.json())
