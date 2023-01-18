1aad41c5273399a9d3008d0c31509eecddd4a6d3
linus <l.pihan@conext-eos.com>
2023-01-18 14:29:21 +0100

    Frontend Rework + Cocktail Filter

--- Code/actions/actions.py
+++ Code/actions/actions.py
@@ -25,3 +25,100 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from sys import displayhook
+from typing import Any, Text, Dict, List
+from pyparsing import nestedExpr
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import json
+import random
+
+alcohol = ""
+taste = ""
+with open(".\data\cocktail.json", "r") as file:
+    data = json.load(file)
+
+class DisplatTaste(Action):
+    
+    def name(self):
+        return 'action_display_taste'
+
+    def run(self, dispatcher,tracker,domain: Dict[Text, Any]):
+        options = [
+            {"title": "Fruchtig", "payload": "fruchtig"},
+            {"title": "Süß", "payload": "suess"},
+            {"title": "Sauer", "payload": "sauer"},
+            {"title": "Bitter", "payload": "bitter"},
+            {"title": "Erfrischend", "payload": "erfrischend"},
+            {"title": "Cremig", "payload": "cremig"}
+        ]
+        dispatcher.utter_message(text = "These are your option", buttons = options)
+        
+class HandleTaste(Action):
+    
+    def name(self):
+        return 'action_handle_taste'
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]):
+        taste = tracker.latest_message["text"]
+        dispatcher.utter_message(text = taste)
+        
+class DisplatAlcohol(Action):
+    
+    def name(self):
+        return 'action_display_alcohol'
+
+    def run(self, dispatcher,tracker,domain: Dict[Text, Any]):
+        options = [
+            {"title": "Mit Alkohol", "payload": "Alcoholic"},
+            {"title": "Ohne Alkohol", "payload": "Non alcoholic"},
+        ]
+        dispatcher.utter_message(text = "These are your option", buttons = options)
+        
+class HandleAlcohol(Action):
+    
+    def name(self):
+        return 'action_handle_alcohol'
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]):
+        alcohol = tracker.latest_message["text"]
+        dispatcher.utter_message(text = alcohol)
+        
+class getCocktails(Action):
+    
+    def name(self):
+        return 'action_get_cocktails'
+    
+    def run(self, dispatcher,tracker,domain: Dict[Text, Any]):
+        results = getCocktails()
+        alcohol = ""
+        taste = ""
+        dispatcher.utter_message(json_message = results)
+        
+        
+def getCocktails():
+    filteredCocktails = []
+    for cocktail in data["drinks"]:
+        cocktailDict = data["drinks"][cocktail]
+        
+        if cocktailDict["strAlcoholic"] == alcohol and cocktailDict["taste1"] == taste:
+
+            filteredCocktails.append(compileCocktailData(cocktailDict))
+            
+    if len(filteredCocktails) < 3: return filteredCocktails
+    return random.sample(filteredCocktails, 3)
+
+def compileCocktailData(cocktail):
+    ingridientsList = []
+    
+    for key in cocktailDict:
+        if key.startswith('strIngredient') and cocktailDict[key] != 'null':
+            ingridientsList.append(cocktail[key])
+    
+    if cocktailDict['strInstructionsDE'] == 'null': instruction = cocktailDict['strInstructions']
+    else: instruction = cocktailDict['strInstructionsDE']
+    
+    return {'name': cocktail['strDrink'], "ingredients": ingridientsList, "url": cocktail['strDrinkThumb'], "instructions": instruction}
--- Code/frontend/script.js
+++ Code/frontend/script.js
@@ -1,18 +1,164 @@
-var url = "http://localhost:5005/webhooks/rest/webhook"
-
-
 function submitForm(e) {
 
-    var textfield = document.getElementById('message-input')
-    var user_message = document.getElementById('message-input').value
+    const textfield = document.getElementById('message-input')
+    const message = textfield.value
+
     e.preventDefault();
     textfield.value = ""
-    appendMessage(user_message, "user")
-    getBotResponse(user_message)
 
+    sendMessage(message)
+
+    displayMessage(message, "user")
 }
 
-function getBotResponse(user_message) {
+function sendMessage(message) {
+    const package = {
+        "sender": "user",
+        "message": message,
+    }
+
+    fetch("http://localhost:5005/webhooks/rest/webhook", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(package)
+    })
+        .then(res => res.json())
+        .then(data => {
+            handleResponse(data[0]);
+        });
+}
+
+function handleResponse(data) {
+    if (data.hasOwnProperty("text")) {
+        console.log("DISPLAY_MESSAGE")
+        displayMessage(data.text, "bot")
+    }
+    if (data.hasOwnProperty("buttons")) {
+        console.log("DISPLAY_BUTTONS")
+        displayButtons(data.buttons)
+    }
+    if (data.hasOwnProperty("custom")) {
+        console.log("DISPLAY_COCKTAILS")
+        displayCocktails(data.custom)
+    }
+}
+
+function displayMessage(message, source) {
+    const scrollContainer = document.getElementById('scroll-container');
+    const messageRow = document.createElement("div")
+    const messageContainer = document.createElement("div")
+    const messageText = document.createElement("p")
+
+    messageContainer.className = 'message-container'
+    messageText.innerText = message
+
+    if (source == "user") {
+        messageRow.className = "user-message-row"
+    }
+    if (source == "bot") {
+        messageRow.className = "bot-message-row"
+    }
+
+    messageContainer.appendChild(messageText)
+    messageRow.appendChild(messageContainer)
+    scrollContainer.appendChild(messageRow)
+}
+
+function displayButtons(buttons) {
+    const scrollContainer = document.getElementById('scroll-container');
+    const buttonContainer = document.createElement("div")
+
+    buttonContainer.className = 'selection-button-container'
+
+    buttons.forEach(btn => {
+        const button = document.createElement("button")
+        button.className = "selction-button"
+        button.dataset.payload = btn.payload
+        button.innerText = btn.title
+        button.addEventListener("click", handleButtonClick)
+        buttonContainer.appendChild(button)
+    })
+
+    scrollContainer.appendChild(buttonContainer)
+}
+
+function displayCocktails(cocktails) {
+    const scrollContainer = document.getElementById('scroll-container');
+    const cocktailsContainer = document.createElement("div")
+
+    cocktailsContainer.className = 'cocktails-container'
+
+    cocktails.forEach(cocktail => {
+        const cocktailContainer = document.createElement("button")
+        const cocktailHeader = document.createElement("div")
+        const cocktailImage = document.createElement("img")
+        const cocktailName = document.createElement("h3")
+        const cocktailBody = document.createElement("div")
+        const cocktailIngredients = document.createElement("div")
+        const cocktailInstructions = document.createElement("p")
+
+        cocktailContainer.className = 'cocktail-container'
+        cocktailHeader.className = 'cocktail-header'
+        cocktailImage.className = 'cocktail-img'
+        cocktailBody.className = 'cocktail-body'
+
+        cocktailImage.src = cocktail.url
+        cocktailName.innerText = cocktail.name
+        cocktail.ingredients.forEach(ingridient => {
+            const cocktailIngridient = document.createElement("p")
+            cocktailIngridient.innerText = ingridient
+            cocktailIngredients.appendChild(cocktailIngridient)
+        })
+        cocktailInstructions.innerText = cocktail.instructions
+
+        cocktailContainer.addEventListener("click", function() {
+            handleCocktailClick(this);
+        }, false)
+
+        cocktailBody.appendChild(cocktailIngredients)
+        cocktailBody.append(cocktailInstructions)
+        cocktailHeader.appendChild(cocktailImage)
+        cocktailHeader.append(cocktailName)
+        cocktailContainer.appendChild(cocktailHeader)
+        cocktailContainer.appendChild(cocktailBody)
+        cocktailsContainer.appendChild(cocktailContainer)
+    })
+
+    scrollContainer.appendChild(cocktailsContainer)
+}
+
+function handleButtonClick(event) {
+    console.log("HANDLEBUTTONCLICK")
+    const selectedButton = event.target
+    const allButtons = Array.from(selectedButton.parentNode.children)
+    console.log(selectedButton.dataset.payload)
+    const taste = selectedButton.dataset.payload
+
+    allButtons.forEach(button => {
+        if(button != selectedButton) button.dataset.selected = false
+        button.disabled = true
+    }); 
+    selectedButton.dataset.selected = true
+
+    sendMessage(taste)
+}
+
+function handleCocktailClick(selectedCocktail) {
+    console.log("HANDLECOCKTAILCLICK")
+
+    const allCocktails = Array.from(selectedCocktail.parentNode.children)
+    console.log(allCocktails)
+
+    allCocktails.forEach(cocktail => {
+        console.log(cocktail)
+        if(cocktail != selectedCocktail) cocktail.dataset.selected = false
+        cocktail.disabled = true
+    })
+    selectedCocktail.dataset.selected = true
+}
+
+
+/* function getBotResponse(user_message) {
 
     var request = new XMLHttpRequest();
     var params = JSON.stringify({
@@ -37,11 +183,9 @@ function getBotResponse(user_message) {
         }
     }
     request.send(params);
-}
+} */
 
-
-
-function getCocktailInfo(data) {
+/* function getCocktailInfo(data) {
     var recCocktails = []
 
     dataArray = data.split(" | ")
@@ -55,29 +199,29 @@ function getCocktailInfo(data) {
         recCocktails.push(recCocktail)
     }
     return recCocktails
-}
+} */
 
-function appendCocktail(data) {
+/* function appendCocktail(data) {
 
     var messageContainer = document.getElementById('scroll-container');
     var ingredientsHTML = ""
     console.log(data)
 
-    for(let cocktail of data) {
+    for (let cocktail of data) {
         let i = 0
         console.log(cocktail[3])
-        for(let ingredient of cocktail[3]) {
-            i ++
+        for (let ingredient of cocktail[3]) {
+            i++
             ingredientsHTML += "<p id='" + cocktail[1] + "-ingredient" + i + "'>" + ingredient + "</p>"
         }
-        
+
         cocktailHTML = "<div class='recipe-container' id='" + cocktail[1] + "-container'><button class='recipe-header' data-selected='none' onclick='toggleRecipe(this)'><img src='" + cocktail[2] + "' class='recipe-img' id='" + cocktail[1] + "-img'><h3 id='" + cocktail[1] + "-title'>" + cocktail[1] + "</h3></button><div class='recipe-detail-body' id='" + cocktail[1] + "-ingredients-container'>" + ingredientsHTML + "</div><div class='recipe-detail-body' id='" + cocktail[1] + "-instructions-container'><p>" + cocktail[4] + "</p></div></div>"
         messageContainer.innerHTML += cocktailHTML
 
     }
 }
 function appendMessage(message, source) {
-    
+
     var messageContainer = document.getElementById('scroll-container');
 
     if (source == "user") {
@@ -102,4 +246,4 @@ function toggleRecipe(button) {
     // recipeButtons.array.forEach(element => {
     //     btn.disabled = true
     // });
-}
+} */
